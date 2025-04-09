808832be416c5789000d37089192d4d3592886cc
Jahnavi709511 <jahnavichandragiri2006@gmail.com>
2025-04-09 13:52:28 +0530

    ok

--- src/main/java/com/klu/DeploymentProject/DeploymentProjectApplication.java
+++ src/main/java/com/klu/DeploymentProject/DeploymentProjectApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeploymentProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeploymentProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentProjectApplication.class, args);
+    }
 }
--- src/main/java/com/klu/DeploymentProject/HomeController.java
+++ src/main/java/com/klu/DeploymentProject/HomeController.java
@@ -1,5 +1,12 @@
 package com.klu.DeploymentProject;
 
-public class HomeController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class HomeController {
+    @GetMapping({"/", "/home", "/status"})
+    public String getStatus() {
+        return "Current date: 09-04-2025";
+    }
 }
